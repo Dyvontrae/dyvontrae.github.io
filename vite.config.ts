0418366c8f59b6953aeecb40760648e2571eff17
Dyvontrae <103144736+Dyvontrae@users.noreply.github.com>
2024-11-30 20:45:01 -0600

    Deployment Success
    Deployed JSON packages correctly on local build, caused a huge headache. Now works with correct packages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed from '/dyvontrae.github.io/' to '/'
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
-});
+  base: '/',
+})
